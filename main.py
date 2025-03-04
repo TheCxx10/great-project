e6bee58c3407ecb3ea5686e171803f12c6c4f68f
Alvar <thecxxroblox@gmail.com>
2025-03-04 14:33:59 +0100

    Add pythagorean calculation

--- main.py
+++ main.py
@@ -24,4 +24,14 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_price": item.price, "item_id": item_id}
+
+@app.get("/pythagorean")
+def pythagorean(a: int | None = None, b: int | None = None, c:int | None = None):
+    if a and b:
+        return {"result": (a**2 + b**2)**0.5}
+    elif a and c:
+        return {"result": (c**2 - a**2)**0.5}
+    elif b and c:
+        return {"result": (c**2 - b**2)**0.5}
+    return {"result": "Invalid input"}
